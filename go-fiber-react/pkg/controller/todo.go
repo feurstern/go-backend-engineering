0219246249bb88cb53315651e366407b6670d382
feurstern <naziincolour123@gmail.com>
2025-05-11 18:14:21 +0700

    #3 refactor the logic of all the controller to the better one

--- go-fiber-react/pkg/controller/todo.go
+++ go-fiber-react/pkg/controller/todo.go
@@ -3,25 +3,52 @@ package controller
 import (
 	"go-fiber-react/pkg/database"
 	"go-fiber-react/pkg/model"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func TodoList(c *fiber.Ctx) error {
+	db := database.DBConnection
 
-	ctx := fiber.Map{
-		"success": false,
+	var todo []model.TodoList
+
+	if err := db.Where("deleted_at IS NULL").Find(&todo).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"msg":     "Faield to retrieve the data ",
+		})
 	}
 
+	return c.Status(200).JSON(fiber.Map{
+		"success": true,
+		"data":    todo,
+	})
+}
+
+func DeleteTodoList(c *fiber.Ctx) error {
+	idParam := c.Params("id")
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": true,
+			"msg":     "Invalid id!",
+		})
+	}
 	db := database.DBConnection
-	var todo *model.TodoParent
+	var todo []model.TodoList
 
-	db.Find(&todo).Where("deleted_at is NULL")
+	if err := db.Model(&model.TodoList{}).Where("id = ?", id).Delete(&todo).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"succes": false,
+			"msg":    "Failed to delete the to do list",
+		})
+	}
 
-	c.Status(200)
-
-	ctx["todo"] = todo
-
-	return c.JSON(ctx)
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"msg":     "The todo list has been deleted succesfully!",
+	})
 
 }
--- go-fiber-react/pkg/controller/user.go
+++ go-fiber-react/pkg/controller/user.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"go-fiber-react/pkg/database"
 	"go-fiber-react/pkg/model"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
@@ -15,21 +16,23 @@ func GetAllUsers(db *gorm.DB) ([]model.User, error) {
 }
 
 func Userlist(c *fiber.Ctx) error {
-	context := fiber.Map{
-		"success": true,
-		"msg":     "User list",
-	}
 
 	db := database.DBConnection
+	var users []model.User
 
-	var users model.User
+	if err := db.Where("deleted_at IS NULL").Find(&users).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"message": "Failed to fetch the fuckjing user data",
+		})
+	}
 
-	db.Find(&users).Where("deleted_at IS NULL")
+	return c.Status(200).JSON(fiber.Map{
+		"msg":     "user list",
+		"success": true,
+		"data ":   users,
+	})
 
-	context["users"] = users
-
-	c.Status(200)
-	return c.JSON(context)
 }
 
 func UpdateUser(c *fiber.Ctx) error {
@@ -45,9 +48,33 @@ func UpdateUser(c *fiber.Ctx) error {
 
 // }
 
-// func DeleteUser(c *fiber.Ctx) error {
+func DeleteUser(c *fiber.Ctx) error {
+	idParam := c.Params("id")
+	id, err := strconv.Atoi(idParam)
 
-// }
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"msg":     "Invalid ID request",
+		})
+	}
+	db := database.DBConnection
+	var user *[]model.User
+
+	if err := db.Model(&model.User{}).Where("id = ?", id).Delete(&user).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"msg":     "Failed to delete the data",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"msg":     "The data has been deleted succesfully!",
+	})
+
+	// if err:= db.where("id = %d")
+}
 
 // func GetAllTodoList(db *gorm.DB) ([]model.TodoList, error) {
 // 	var todo_list []model.TodoParent
