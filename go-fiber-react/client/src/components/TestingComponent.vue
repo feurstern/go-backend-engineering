b1126465345882c7697839fb8b5eb3d5d5aea02e
feurstern <naziincolour123@gmail.com>
2025-05-11 18:46:59 +0700

    #6 setup the client

---
+++ go-fiber-react/client/src/components/TestingComponent.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { title } from 'process';
+import { Skills } from './models';
+
+interface TestingProps {
+  title: string;
+  skill: Skills[];
+}
+
+const props = withDefaults(defineProps<TestingProps>(), {
+  skill: () => [],
+});
+</script>
+
+<template>
+  <p>title {{ title }}</p>
+  <ul>
+    <li v-for="x in skill" :key="x.id">Language : {{ x }}</li>
+  </ul>
+</template>
