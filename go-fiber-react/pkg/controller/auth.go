7301e759ec2566bfd924da53d49f1f75676e9ea7
feurstern <naziincolour123@gmail.com>
2025-05-25 12:31:06 +0700

    #6 implemented the hashed password function

--- go-fiber-react/pkg/controller/auth.go
+++ go-fiber-react/pkg/controller/auth.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"go-fiber-react/pkg/database"
 	"go-fiber-react/pkg/model"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,14 +11,22 @@ func Registration(c *fiber.Ctx) error {
 
 	payload := model.RegisterPayload{}
 
+	var users model.User
 	if err := c.BodyParser(&payload); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"success": false,
-			"message": "Invalid Request",
-		})
+			"message": "Invalid Request"})
 	}
 
-	// db := database.DBConnection
+	db := database.DBConnection
+
+	if err := db.Create(&users{
+		users.Username: payload.Username,
+		users.Email:    payload.Email,
+		users.Password: payload.Password,
+	}); err != nil {
+
+	}
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"success": true,
